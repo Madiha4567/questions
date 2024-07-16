0944febfb264b65e47552abb016d22f60cb31a80
Madiha4567 <madihanawaz939@gmail.com>
2024-07-16 20:49:26 +0500

    question 11

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-//excercise 10
-// my name is madiha 
-// date : 08/03/2024
-//this program will run simple code just like hello 
-console.log("hello world simple program");
+
--- app.ts
+++ app.ts
@@ -1,5 +1,4 @@
-//excercise 10
-// my name is madiha 
-// date : 08/03/2024
-//this program will run simple code just like hello 
-console.log(`hello world simple program`);
+let members: string []=[`mariam`,`rahmeen`,`aroona`, `daniya`,`fatima`,`sobia`];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
