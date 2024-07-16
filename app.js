39162b776d49a89c158fdbe175b0bb20076dc109
Madiha4567 <madihanawaz939@gmail.com>
2024-07-16 20:50:22 +0500

    question12

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+//exercise 11
+//let members: string[]=[`mariam`, `rahmeen`,`daniya`,`aroona`,`fatima`,];
+//for(let i=0; i<members.length; i++){
+//  console.log(members[i]);
+//}
+//exercise 12
+var members = ["mariam", "rahmeen", "daniya", "aroona", "fatima",];
+var message = "tomorrow is my birthay:";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
--- app.ts
+++ app.ts
@@ -1,4 +1,12 @@
-let members: string []=[`mariam`,`rahmeen`,`aroona`, `daniya`,`fatima`,`sobia`];
+//exercise 11
+//let members: string[]=[`mariam`, `rahmeen`,`daniya`,`aroona`,`fatima`,];
+//for(let i=0; i<members.length; i++){
+  //  console.log(members[i]);
+//}
+//exercise 12
+let members: string[] = [`mariam`, `rahmeen`,`daniya`,`aroona`,`fatima`,];
+let message: string =  `tomorrow is my birthay:`;
 for(let i=0; i<members.length; i++){
-    console.log(members[i]);
+    console.log( message  +  members [i]);
 }
+    
